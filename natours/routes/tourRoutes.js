36bcad26fa75569b40ca3d855d29583606dd6e4c
solygambas <solygambas@gmail.com>
2020-07-06 08:48:22 +0200

    add aggregate, middlewares and basic validation

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const {
-  aliasTopTours,
   getAllTours,
-  createTour,
   getTour,
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
+  createTour,
   updateTour,
   deleteTour,
 } = require("../controllers/tourController");
@@ -12,6 +14,8 @@ const {
 const router = express.Router();
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
+router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router
   .route("/")
