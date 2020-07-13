337c97e4f1483ac1a72e1ba7ccc7de1513a6ed72
solygambas <solygambas@gmail.com>
2020-07-13 10:24:06 +0200

    fix logout

--- natours/public/js/login.js
+++ natours/public/js/login.js
@@ -28,7 +28,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: "GET",
-      url: "http://localhost:3000/api/v1/users/logout",
+      url: "/api/v1/users/logout",
     });
     if ((res.data.status = "success")) location.reload(true);
   } catch (err) {
