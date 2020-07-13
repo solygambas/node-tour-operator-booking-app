687abcb27939106f29a1d8936db4dfa8c101e574
solygambas <solygambas@gmail.com>
2020-07-13 08:28:27 +0200

    add node-farm readme

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -41,6 +41,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, req, res);
 });
+
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
   // 1) Check if email and password exist
