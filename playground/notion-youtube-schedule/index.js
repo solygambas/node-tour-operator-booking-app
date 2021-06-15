cfbd90afa62cd38691a5d0e524974f391ebf704e
solygambas <51904909+solygambas@users.noreply.github.com>
2021-06-15 07:59:56 +0200

    add notion frontend

--- playground/notion-youtube-schedule/index.js
+++ playground/notion-youtube-schedule/index.js
@@ -1,6 +1,19 @@
+const express = require("express");
 const getVideos = require("./services/notion");
+const PORT = process.env.PORT || 5000;
 
-(async () => {
-  const notionVideos = await getVideos();
-  console.log(notionVideos);
-})();
+// (async () => {
+//   const notionVideos = await getVideos();
+//   console.log(notionVideos);
+// })();
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/videos", async (req, res) => {
+  const videos = await getVideos();
+  res.json(videos);
+});
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
