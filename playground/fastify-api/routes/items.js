762a8c59906e661e3ea7123436b25bb930037a71
solygambas <51904909+solygambas@users.noreply.github.com>
2021-06-21 08:33:26 +0200

    add clean routes

--- playground/fastify-api/routes/items.js
+++ playground/fastify-api/routes/items.js
@@ -1,17 +1,47 @@
 const items = require("../Items");
 
-function itemRoutes(fastify, options, done) {
-  // Get all items
-  fastify.get("/items", (req, reply) => {
-    reply.send(items);
-  });
+const Item = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+  },
+};
 
-  // Get single items
-  fastify.get("/items/:id", (req, reply) => {
+// Options for get all items
+const getItemsOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: Item,
+      },
+    },
+  },
+  handler: function (req, reply) {
+    reply.send(items);
+  },
+};
+
+const getItemOptions = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: function (req, reply) {
     const { id } = req.params;
     const item = items.find((item) => id === item.id);
     reply.send(item);
-  });
+  },
+};
+
+function itemRoutes(fastify, options, done) {
+  // Get all items
+  fastify.get("/items", getItemsOptions);
+
+  // Get single items
+  fastify.get("/items/:id", getItemOptions);
 
   done();
 }
