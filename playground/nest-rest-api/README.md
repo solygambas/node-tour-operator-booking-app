b066a8de0c5397f387bd0baba79b32afffe4f4b5
solygambas <51904909+solygambas@users.noreply.github.com>
2021-06-28 07:29:28 +0200

    add controller

--- playground/nest-rest-api/README.md
+++ playground/nest-rest-api/README.md
@@ -2,4 +2,8 @@
 
 A CRUD REST API using NestJS, MongoDB and Mongoose.
 
+## REST Client
+
+The file test.http can be used to make requests if you are using the [VSCode Rest Client extension](https://marketplace.visualstudio.com/items?itemName=humao.rest-client)
+
 Based on [NestJS Crash Course](https://www.youtube.com/watch?v=wqhNoDE6pb4) by Brad Traversy (2019).
--- playground/nest-rest-api/src/items/items.controller.ts
+++ playground/nest-rest-api/src/items/items.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  //   Req,
+  //   Res,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+// import { Request, Response } from 'express';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  //   @Get()
+  //   findAll(@Req() req: Request, @Res() res: Response): Response {
+  //     console.log(req.url);
+  //     return res.send('Hello world');
+  //   }
+  @Get()
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+}
