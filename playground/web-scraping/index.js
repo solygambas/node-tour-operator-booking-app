ac174ae2829a4ce0c5ba7f232185b61f5ba46e02
solygambas <51904909+solygambas@users.noreply.github.com>
2021-08-03 10:20:57 +0200

    add node-cron

--- playground/web-scraping/index.js
+++ playground/web-scraping/index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
+const cron = require("node-cron");
 
 async function start() {
   const browser = await puppeteer.launch();
@@ -15,16 +16,36 @@ async function start() {
   // });
   // await fs.writeFile("names.txt", names.join("\r\n"));
 
-  const photos = await page.$$eval("img", (images) => {
-    return images.map((image) => image.src);
-  });
+  // await page.click("#clickme");
+  // const clickedData = await page.$eval(
+  //   "#data",
+  //   (element) => element.textContent
+  // );
+  // console.log(clickedData);
 
-  for (const photo of photos) {
-    const imagePage = await page.goto(photo);
-    await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
-  }
+  // const photos = await page.$$eval("img", (images) => {
+  //   return images.map((image) => image.src);
+  // });
+
+  await page.type("#ourfield", "blue");
+  await Promise.all([page.click("#ourform button"), page.waitForNavigation()]);
+  const info = await page.$eval("#message", (element) => element.textContent);
+  console.log(info);
+
+  // for (const photo of photos) {
+  //   const imagePage = await page.goto(photo);
+  //   await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
+  // }
 
   await browser.close();
 }
 
 start();
+
+// schedule task
+// setInterval(start, 5000);
+
+// with cron
+// cron.schedule("*/5 * * * * *", start);
+
+// on a linux server, search for cron jobs
