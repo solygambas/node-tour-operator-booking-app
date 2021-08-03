981e5eb1b619e3afe24e867baadd3d07f826ae8b
solygambas <51904909+solygambas@users.noreply.github.com>
2021-08-03 09:43:40 +0200

    add screenshot, scraping names and jpgs

--- playground/web-scraping/index.js
+++ playground/web-scraping/index.js
@@ -1,10 +1,29 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 
 async function start() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://learnwebcode.github.io/practice-requests/");
-  await page.screenshot({ path: "screenshot.png" });
+
+  // await page.screenshot({ path: "screenshot.png", fullPage: true });
+
+  // const names = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll(".info strong")).map(
+  //     (name) => name.textContent
+  //   );
+  // });
+  // await fs.writeFile("names.txt", names.join("\r\n"));
+
+  const photos = await page.$$eval("img", (images) => {
+    return images.map((image) => image.src);
+  });
+
+  for (const photo of photos) {
+    const imagePage = await page.goto(photo);
+    await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
+  }
+
   await browser.close();
 }
 
