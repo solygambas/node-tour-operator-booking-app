0943adb7f8bf7b2f9d49927f0476d483bf8be629
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-20 08:23:59 +0100

    add firestore config

--- playground/firebase-webpack/README.md
+++ playground/firebase-webpack/README.md
@@ -5,5 +5,7 @@ A quick introduction to Firebase 9.
 ## Features
 
 - setting up Webpack and Firebase.
+- understanding Firebase 9 modular approach.
+- creating a Firestore database and fetching data.
 
 Based on [Getting Started with Firebase 9](https://www.youtube.com/playlist?list=PL4cUxeGkcC9jERUGvbudErNCeSZHWUVlb) by Shaun Pelling - The Net Ninja (2021).
--- playground/firebase-webpack/src/index.js
+++ playground/firebase-webpack/src/index.js
@@ -1,1 +1,26 @@
-console.log("hello from index.js");
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+import { firebaseConfig } from "./config";
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "books");
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
