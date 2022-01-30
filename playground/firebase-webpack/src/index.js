f8025843f52831fca2690c8157761731bd13a8b5
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-30 11:43:32 +0100

    add creating and deleting

--- playground/firebase-webpack/src/index.js
+++ playground/firebase-webpack/src/index.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 import { firebaseConfig } from "./config";
 
@@ -24,3 +31,25 @@ getDocs(colRef)
   .catch((err) => {
     console.log(err.message);
   });
+
+// add documents
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  }).then(() => {
+    addBookForm.reset();
+  });
+});
+
+// delete documents
+const deleteBookForm = document.querySelector(".delete");
+deleteBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const docRef = doc(db, "books", deleteBookForm.id.value);
+  deleteDoc(docRef).then(() => {
+    deleteBookForm.reset();
+  });
+});
