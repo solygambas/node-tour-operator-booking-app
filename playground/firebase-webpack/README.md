dbbf030b550a71f3165d00886deae0a8c0bb8c79
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-30 12:02:24 +0100

    add real time listener and Firestore queries

--- playground/firebase-webpack/README.md
+++ playground/firebase-webpack/README.md
@@ -7,5 +7,8 @@ A quick introduction to Firebase 9.
 - setting up Webpack and Firebase.
 - understanding Firebase 9 modular approach.
 - creating a Firestore database and fetching data.
+- adding and deleting data.
+- setting up a real time listener.
+- using Firestore queries.
 
 Based on [Getting Started with Firebase 9](https://www.youtube.com/playlist?list=PL4cUxeGkcC9jERUGvbudErNCeSZHWUVlb) by Shaun Pelling - The Net Ninja (2021).
--- playground/firebase-webpack/src/index.js
+++ playground/firebase-webpack/src/index.js
@@ -2,10 +2,13 @@ import { initializeApp } from "firebase/app";
 import {
   getFirestore,
   collection,
-  getDocs,
+  // getDocs,
   addDoc,
   deleteDoc,
   doc,
+  onSnapshot,
+  query,
+  where,
 } from "firebase/firestore";
 
 import { firebaseConfig } from "./config";
@@ -19,18 +22,39 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, "books");
 
+// queries
+const q = query(colRef, where("author", "==", "patrick rothfuss"));
+
 // get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let books = [];
-    snapshot.docs.forEach((doc) => {
-      books.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(books);
-  })
-  .catch((err) => {
-    console.log(err.message);
+// getDocs(colRef)
+//   .then((snapshot) => {
+//     let books = [];
+//     snapshot.docs.forEach((doc) => {
+//       books.push({ ...doc.data(), id: doc.id });
+//     });
+//     console.log(books);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+// get real time data
+// onSnapshot(colRef, (snapshot) => {
+//   let books = [];
+//   snapshot.docs.forEach((doc) => {
+//     books.push({ ...doc.data(), id: doc.id });
+//   });
+//   console.log(books);
+// });
+
+// get real time query
+onSnapshot(q, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
   });
+  console.log(books);
+});
 
 // add documents
 const addBookForm = document.querySelector(".add");
