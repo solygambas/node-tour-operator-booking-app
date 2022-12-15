e8a7afe17174240fd6251da4b106ea59e506b0ff
solygambas <51904909+solygambas@users.noreply.github.com>
2022-12-15 15:17:47 +0100

    add openai request

---
+++ playground/ai-image-gen/README.md
@@ -0,0 +1,16 @@
+# AI Image Generator
+
+A web app that generates images from scratch based on the entered text, using OpenAI and the DALL-E model.
+
+## Features
+
+- setting up and installing dependencies.
+- creating an Express server and adding env variables.
+- defining a route and a controller.
+- using OpenAI library.
+- requesting body data.
+- setting up a basic frontend.
+- generating an image based on user input.
+- displaying image in DOM.
+
+Based on [Build An AI Image Generator With OpenAI & Node.js](https://www.youtube.com/watch?v=fU4o_BKaUZE) by Brad Traversy (2022).
---
+++ playground/ai-image-gen/controllers/openaiController.js
@@ -0,0 +1,34 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, res) => {
+  try {
+    const response = await openai.createImage({
+      prompt: "Polar bear on ice skates",
+      n: 1,
+      size: "512x512",
+    });
+    const imageUrl = response.data.data[0].url;
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+    res.status(400).json({
+      success: false,
+      error: "The image could not be generated",
+    });
+  }
+};
+
+module.exports = { generateImage };
